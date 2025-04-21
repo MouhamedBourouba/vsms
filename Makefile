f7d0f11ffa82e78405ff3d1c2e6e36240d4713cd
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-04-22 00:15:36 +0100

    grpc server impl

--- Makefile
+++ Makefile
@@ -2,16 +2,15 @@ all: bin/api protoc
 
 bin/api: $(wildcard cmd/api/*.go)
 	go build -o bin/api ./cmd/api 
+	./bin/api
 
 protoc: .protoc.stamp
-
 .protoc.stamp: $(wildcard api/proto/*.proto)
-	protoc \
-		--go_out=api/generated \
-		--go-grpc_out=api/generated \
-		api/proto/*.proto
+	protoc --go_out=api/generated --go-grpc_out=api/generated api/proto/*.proto
 	touch .protoc.stamp
 
 clean:
 	rm -rf bin
 	rm -rf api/generated
+
+.PHONY: clean all
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,66 @@
 package main
 
-func startGrpcApi() {
+import (
+	"fmt"
+
+	"log"
+	"net"
+	"time"
+
+	chatpb "github.com/MouhamedBourouba/vsms/api/generated/chatapi"
+	"github.com/nats-io/nats.go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const port = 8080
+
+type ChatServer struct {
+  chatpb.UnimplementedChatServiceServer
+}
+
+
+
+func (*ChatServer) ListenToRoom(req *chatpb.ListenRequest, stream chatpb.ChatService_ListenToRoomServer) error {
+	fmt.Println("Client subscribed to room:", req.RoomId)
+
+	messages := []string{"yoyoyo", "yoyoyo", "yoyoyo", "yoyoyo", "yoyoyo", "yoyoyo"}
+
+	for _, msg := range messages {
+		err := stream.Send(&chatpb.ListenResponse{Message: msg})
+		if err != nil {
+			return fmt.Errorf("failed to send message: %w", err)
+		}
+		time.Sleep(time.Second * 3)
+	}
+
+	select {}
+}
+
+func startGrpcApi() error {
+  lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+  if err != nil {
+    log.Fatal("Could not start grpc server ", err)
+  }
+  
+  chatServer := ChatServer{}
+  grpcServer := grpc.NewServer()
+  
+  chatpb.RegisterChatServiceServer(grpcServer, &chatServer)
+  reflection.Register(grpcServer)
+  
+  return grpcServer.Serve(lis)
 }
 
 func main() {
-  println("the server")
-  startGrpcApi()
+  conn, err := nats.Connect(nats.DefaultURL)
+  defer conn.Close()
+
+
+  if err != nil {
+    panic("Could not connect to nats client")
+  }
+
+  println("server running")
+  log.Fatal(startGrpcApi())
 }
